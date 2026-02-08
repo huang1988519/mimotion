9dba632c5e936678904b625d01d2c1ec3af5098f
Wally Huang <huang1988519@gmail.com>
2026-02-08 12:57:05 +0800

    从环境变量读取wechat

--- main.py
+++ main.py
@@ -301,7 +301,7 @@ if __name__ == "__main__":
             push_plus_token=config.get('PUSH_PLUS_TOKEN'),
             push_plus_hour=config.get('PUSH_PLUS_HOUR'),
             push_plus_max=get_int_value_default(config, 'PUSH_PLUS_MAX', 30),
-            push_wechat_webhook_key=config.get('PUSH_WECHAT_WEBHOOK_KEY'),
+            push_wechat_webhook_key=config.get('PUSH_WECHAT_WEBHOOK_KEY', os.environ.get("PUSH_WECHAT_WEBHOOK_KEY")),
             telegram_bot_token=config.get('TELEGRAM_BOT_TOKEN'),
             telegram_chat_id=config.get('TELEGRAM_CHAT_ID')
         )
